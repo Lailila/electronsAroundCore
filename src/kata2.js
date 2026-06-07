db7bf6c9c61e7760fb841525320dc6544253386e
Michael Souhrada <michaelsouh@gmail.com>
2026-06-07 14:44:30 +0200

    simplest successful code for last todo Michael

--- src/kata2.js
+++ src/kata2.js
@@ -1,19 +1,12 @@
 function electronsAroundCore() {}
 
 electronsAroundCore.prototype.add = function electronsAroundCore(dice) {
-    if(JSON.stringify(dice) === JSON.stringify([1,2,3,4,5]))
-    {
-        return 6;
-    }
-    else if(JSON.stringify(dice) === JSON.stringify([2,2,3,3])){
-      return 4;
-    }
     let sum = 0;
     for(let i = 0; i < dice.length; i++)
     {
         if(dice[i]%2 == 1)
         {
-            sum = sum + 1;
+            sum = sum + dice[i] - 1;
         }
     }
     return sum;  
